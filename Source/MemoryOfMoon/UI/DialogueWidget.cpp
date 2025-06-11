1ddf55dfceab16f34a0775e268ad29510c5d091a
meo-young <meoyoung000@naver.com>
2025-06-11 23:40:31 +0900

    [Add] DialogueWidget 클래스 추가

---
+++ Source/MemoryOfMoon/UI/DialogueWidget.cpp
@@ -0,0 +1,2 @@
+#include "UI/DialogueWidget.h"
+
---
+++ Source/MemoryOfMoon/UI/DialogueWidget.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DialogueWidget.generated.h"
+
+UCLASS()
+class MEMORYOFMOON_API UDialogueWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
