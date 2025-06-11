6fdba722f5fccb3df8d042cd8064f05dc926be4b
meo-young <meoyoung000@naver.com>
2025-06-11 22:59:40 +0900

    [Update] LevelSequence에 필요한 모듈 추가

--- Source/MemoryOfMoon/MemoryOfMoon.Build.cs
+++ Source/MemoryOfMoon/MemoryOfMoon.Build.cs
@@ -8,12 +8,19 @@ public class MemoryOfMoon : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput",
+			"LevelSequence",
+			"MovieScene"
+		});
 		
 		PublicIncludePaths.AddRange(new string[] { "MemoryOfMoon" });
-
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
+		
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
