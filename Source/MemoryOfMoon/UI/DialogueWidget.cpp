5e146893cc358c187a6d50c3db02b637c26b25fc
meo-young <meoyoung000@naver.com>
2025-06-21 12:36:40 +0900

    [Update] 대화창 구현

--- Source/MemoryOfMoon/UI/DialogueWidget.cpp
+++ Source/MemoryOfMoon/UI/DialogueWidget.cpp
@@ -1,2 +1,50 @@
 #include "UI/DialogueWidget.h"
+#include "MemoryOfMoon.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
 
+
+void UDialogueWidget::ShowDialogueWidget(const FString& InCharacterEmotion, const FString& InCharacterName, const FString& InDialogue)
+{
+	if (IsValid(Image_CharacterSprite))
+	{
+		// 캐릭터 이름의 공백을 제거한다.
+		const FString TrimmedCharacterName = InCharacterName.Replace(TEXT(" "), TEXT(""));
+
+		// 경로 생성: /Game/PaperAssets/Illust/캐릭터이름/T_캐릭터이름_감정
+		const FString TexturePath = FString::Printf(TEXT("/Game/PaperAssets/Illust/%s/T_%s_%s"), 
+											*TrimmedCharacterName, 
+											*TrimmedCharacterName, 
+											*InCharacterEmotion);
+
+		// 경로로 텍스처를 로드한다.
+		UTexture2D* CharacterSpriteImage = LoadObject<UTexture2D>(nullptr, *TexturePath);
+		if (!IsValid(CharacterSpriteImage)) return;
+
+		// 로드에 성공한 경우 UI에 출력한다.
+		Image_CharacterSprite->SetBrushFromTexture(CharacterSpriteImage);
+	}
+
+	if (IsValid(Text_CharacterName))
+	{
+		Text_CharacterName->SetText(FText::FromString(InCharacterName));	
+	}
+
+	if (IsValid(Text_Dialogue))
+	{
+		Text_Dialogue->SetText(FText::FromString(InDialogue));
+	}
+
+	if (!IsInViewport())
+	{
+		AddToViewport();	
+	}
+}
+
+void UDialogueWidget::HideDialogueWidget()
+{
+	if (IsInViewport())
+	{
+		RemoveFromParent();
+	}
+}
--- Source/MemoryOfMoon/UI/DialogueWidget.h
+++ Source/MemoryOfMoon/UI/DialogueWidget.h
@@ -4,9 +4,29 @@
 #include "Blueprint/UserWidget.h"
 #include "DialogueWidget.generated.h"
 
+class UImage;
+class UTextBlock;
+
 UCLASS()
 class MEMORYOFMOON_API UDialogueWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	void ShowDialogueWidget(const FString& InCharacterEmotion, const FString& InCharacterName, const FString& InDialogue);
+	void HideDialogueWidget();
+	
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_CharacterSprite;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_Arrow;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_CharacterName;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_Dialogue;
 	
 };
